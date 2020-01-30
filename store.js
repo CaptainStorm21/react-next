d27470c7c39032fdaaae081fff5f006d1c680b68
Captain Storm <CaptainStorm21@users.noreply.github.com>
2020-01-29 22:41:23 -0800

    reducer set up

--- store.js
+++ store.js
@@ -3,10 +3,41 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import thunkMiddleware from 'redux-thunk';
 import data from './data/data.json';
 
-//setting up an intial this.state.
+//setting up an initial this.state.
 const startState = {
     cards: []
 }
+
+//actions
+export const initialCards =  () => {
+    return {
+        type: 'INITIALCARDS',
+        cards: data
+    }
+}
+
+export const addItem = ( item ) => {
+    return {
+        type: 'ADD',
+        item
+    }
+}
+
+//reducers
+export const reducer = (state = initialState, action ) => {
+    switch ( action.type ) {
+        case 'INITIALCARDS':
+            return {
+                cards : action.cards
+            }
+        case 'ADD':
+            return {
+                ...state, 
+                cards: [ ...state.cards, action.item ]
+            }
+        default: return state
+    }
+} 
 
 //create store
 export const initStore = ( initialState = startState ) => {
